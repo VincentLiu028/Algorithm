0303035e9404878ced67f1f0433a39b23da0587c
Vincent Liu <vincentl@microsoft.com>
2017-01-23 18:15:30 +0800

    Merge Sort

---
+++ MergeSort/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test();
+        }
+
+
+        public static void MergeSort(int[] input, int start, int end)
+        {
+            if(start < end)
+            {
+                int middle = (start + end)/2;
+                MergeSort(input, start, middle);
+                MergeSort(input, middle+1, end);
+                Merge(input, start, middle, end);
+            }
+        }
+
+        public static void Merge(int[] input, int start, int middle, int end)
+        {
+            if(input ==null && input.Length <=1)
+            {
+                return;
+            }
+            int leftLength = middle - start + 2;
+            int rightLength = end - middle  + 1;
+            int[] left = new int[leftLength];
+            int[] right = new int[rightLength];
+            for(int i = start; i<= middle; i++)
+            {
+                left[i-start] = input[i];
+            }
+
+            left[leftLength-1] = int.MaxValue;
+            for(int j= middle+1; j<= end; j++)
+            {
+                right[j-middle-1] = input[j];
+            }
+            right[rightLength-1] = int.MaxValue;
+            int inputIndex = start;
+            int leftIndex = 0;
+            int rightIndex = 0;
+            while(inputIndex <=end)
+            {
+                if(left[leftIndex]< right[rightIndex])
+                {
+                    input[inputIndex] = left[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    input[inputIndex] = right[rightIndex];
+                    rightIndex++;
+                }
+                inputIndex++;
+            }
+        }
+
+        public static void Test()
+        {
+            int[] input1 = {2,5,3};
+            MergeSort(input1, 0, 2);
+            Console.WriteLine(string.Join(", ", input1));
+            int[] input2 = {6,4,2,1,0,-1};
+            MergeSort(input2, 0,input2.Length-1);
+            Console.WriteLine(string.Join(", ", input2));
+            int[] input4 = {1,1,1,1,1};
+            MergeSort(input4, 0 ,input4.Length-1);
+            Console.WriteLine(string.Join(", ", input4));
+            int[] input3 = {1};
+            MergeSort(input3, 0,input3.Length-1);
+            Console.WriteLine(string.Join(", ", input3));
+            int[] input5 = {1,2,6,3,4,5};
+            MergeSort(input5, 0,input5.Length-1);
+            Console.WriteLine(string.Join(", ", input5));
+
+        }
+    }
+}
---
+++ SelectSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            test();
+        }
+
+        public static int[] selectSort(int[] input)
+        {
+            if (input == null || input.Length == 0)
+            {
+                return input;
+            }
+
+            int minValueIndex = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i; j < input.Length; j++)
+                {
+                    if (input[j] < input[minValueIndex])
+                    {
+                        minValueIndex = j;
+                    }
+                }
+
+                if (i != minValueIndex)
+                {
+                    var temp = input[i];
+                    input[i] = input[minValueIndex];
+                    input[minValueIndex] = temp;
+                }
+                minValueIndex = i + 1;
+            }
+
+            return input;
+        }
+
+        public static void test()
+        {
+            int[] input = { 1, 2, 3, 4, 5, 6, 8 };
+            Console.WriteLine(String.Join(", ", selectSort(input)));
+            int[] input1 = { 5, -1, 4, 3, -5, 11, 5 };
+            Console.WriteLine(String.Join(", ", selectSort(input1)));
+            int[] input2 = { 9, 4, 2, 1, -5 };
+            Console.WriteLine(String.Join(", ", selectSort(input2)));
+            int[] input3 = { -1, -1, -1, -1 };
+            Console.WriteLine(String.Join(", ", selectSort(input3)));
+        }
+    }
+}
